49f787ca9ff81544641f527077408877a8a0ead2
Alex <alex.518478@gmail.com>
2022-05-17 16:28:06 +0200

    backup just in case i'm dumb

--- entity.cpp
+++ entity.cpp
@@ -1,38 +1,12 @@
 #include "entity.h"
 
 
-entity::entity(){}
+entity::entity() {}
 
-entity::entity(color,tower,position,taille)
+entity::entity(sf::RectangleShape* body)
 {
-	this->body.setFillColor(color);
-	this->tower = tower;
-	body.setPosition(position);
-	body.set   //set la taille
+	this->body = body;
 }
 
 entity::~entity()
 {}
-
-entity::deplacement(){
-
-	if(!collide)
-	{
-	direction = this.body.position() - tower.position();
-	if(direction.x < 0){
-		body.move(direction.x%2+1*50,0)
-	}
-	else {
-		body.move(direction.x%2-1*50,0)
-	}	
-		if (body.getGlobalBounds().intersects(tour.getGlobalBounds()))
-		{
-			std::cout << "Touch�";
-			collide = true;
-			rectangleGauche.setFillColor(sf::Color::Red);
-		}
-	}
-	
-	
-
-}
